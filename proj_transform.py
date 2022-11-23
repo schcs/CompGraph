6cc0bff7f9610d9902872f1d77733613468606bf
Csaba Schneider <csaba.schneider@gmail.com>
2022-11-23 13:24:41 -0300

    added main_persp_1

--- proj_transform.py
+++ proj_transform.py
@@ -11,27 +11,52 @@ class ProjPlane:
         self.y0 = y0
     
     def coordinates_of_point( self, Q ):
-        proj = perspective_projection_matrix( [coord[0], coord[1], coord[2], 0 ], 
-                                                [coord[0], coord[1], coord[2],0] )
+
+        proj = perspective_projection_matrix( [self.coord[0], self.coord[1], self.coord[2], 0 ], 
+                                                [self.coord[0], self.coord[1], self.coord[2],0] )        
         Q0 = Q@proj
-        Q0 = [ Q0[0], Q0[1], Q0[2] ]
+        Q0 = [ Q0[0]/Q0[3], Q0[1]/Q0[3], Q0[2]/Q0[3] ]
         return dot( Q0, self.x0 ), dot( Q0, self.y0 )
 
-    def rotate( self, rot ):
-        self.coord = self.coord@rot 
-        print( rot[[0,1,2]][[0,1,2]] )
-        self.x0 = self.x0@rot[ix_([0,1,2],[0,1,2])]
-        self.y0 = self.y0@rot[ix_([0,1,2],[0,1,2])]
+    def rotate( self, dir, angle, C ):
+
+        projC = C@perspective_projection_matrix( [ self.coord[0], self.coord[1], self.coord[2], 0], 
+                    self.coord )
+        #print( "projC", projC, "C", C )
+        #print( "dist0: ", ( (projC[0]/projC[3]-C[0]/C[3])**2 + (projC[1]/projC[3]-C[1]/C[3])**2 + 
+        #                (projC[2]/projC[3]-C[2]/C[3])**2 )**(1/2))
+                    
+        rot = matrix_rotation_3D_homog( angle, n = dir, C = C )
+        rot_orig = matrix_rotation_3D_homog( angle, n = dir )
+
+        self.coord = self.coord@array(transpose( matrix( rot ))**-1) 
+        self.x0 = self.x0@rot_orig[ix_([0,1,2],[0,1,2])]
+        self.y0 = self.y0@rot_orig[ix_([0,1,2],[0,1,2])]
+
+        assert abs(dot( self.x0, self.y0 )) < 10**-6
+        assert abs(dot( self.x0, self.coord[[0,1,2]] )) < 10**-6
+        assert abs(dot( self.y0, self.coord[[0,1,2]] )) < 10**-6
+
+        projC = C@perspective_projection_matrix( [ self.coord[0], self.coord[1], self.coord[2], 0], 
+                    self.coord )
+
+        print( "dist0: ", ( (projC[0]/projC[3]-C[0]/C[3])**2 + (projC[1]/projC[3]-C[1]/C[3])**2 + 
+                        (projC[2]/projC[3]-C[2]/C[3])**2 )**(1/2))
+        
     
     def translate( self, trans ):
-        self.coord = self.coord@trans
+        self.coord = self.coord@transpose( trans )
 
+        assert abs(dot( self.x0, self.y0 )) < 10**-6
+        assert abs(dot( self.x0, self.coord[[0,1,2]] )) < 10**-6
+        assert abs(dot( self.y0, self.coord[[0,1,2]] )) < 10**-6
 
 def perspective_projection_matrix( C, P ):    
-    return array( transpose( matrix( P.coord ))*matrix( C ) - dot( P.coord, C )*identity( 4 ))
+    return array( transpose( matrix( P ))*matrix( C ) - dot( P, C )*identity( 4 ))
 
 
-def matrix_rotation_3D_homog( theta, n = (1,0,0) ):
+def matrix_rotation_3D_homog( theta, n = (1,0,0), C = (0,0,0) ):
+
     norm = sqrt(n[0]**2 + n[1]**2 + n[2]**2)
     n /= array(norm) 
     nn = transpose( matrix( n ))*matrix( n )
@@ -40,7 +65,12 @@ def matrix_rotation_3D_homog( theta, n = (1,0,0) ):
                  [-n[1],n[0],0]])
     mat = pad( array( cos(theta)*identity( 3 )+sin(theta)*nx+(1-cos(theta))*nn ), [(0,1)] )
     mat[3,3] = 1
-    return mat
+
+    if C == (0,0,0):
+        return mat
+    else:
+        trans = matrix( matrix_translation_3D_homog( [C[0]/C[3],C[1]/C[3],C[2]/C[3]] ))
+        return array(trans**-1*mat*trans) 
 
 def matrix_translation_3D_homog( vec ):
     mat = identity( 4 )
